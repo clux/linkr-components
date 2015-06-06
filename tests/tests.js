28493a562d6660d0647648e82889569236fb2851
clux <analsandblaster@gmail.com>
2015-06-06 11:27:59 +0100

    add baseUrl property to linkr-list

--- tests/tests.js
+++ tests/tests.js
@@ -4,6 +4,7 @@ test("link", function (t) {
   var tag = document.createElement('ul', 'linkr-list');
   t.ok(tag, "createElement worked");
   tag.id = "testid";
+  tag.baseurl = 'c/';
   tag.links = [
     {
       id: '12323',
@@ -11,23 +12,20 @@ test("link", function (t) {
       title: "Scott Pilgrim Vs. The World The Game OST",
       date: new Date(),
       category: 'beautiful',
-      categoryUrl: "http://clux.org/c/beautiful"
     },
     {
       id: '34545',
       url: "https://github.com/clux/tournament",
       title: "tournament module page",
       date: new Date(),
-      category: 'helpful',
-      categoryUrl: "http://clux.org/c/helpful"
+      category: 'helpful'
     },
     {
       id: '34125',
       url: "git://github.com/clux/interlude.git",
       title: "functional javascript library",
       date: new Date(),
-      category: 'helpful',
-      categoryUrl: "http://clux.org/c/helpful"
+      category: 'helpful'
     }
   ];
   document.body.appendChild(tag);
